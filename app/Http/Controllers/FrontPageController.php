734817ef1dbe861abaa56f430d4c8353483bf30e
octopus <ahyana1995@gmail.com>
2017-10-16 09:44:08 +0700

    add login middleware

--- app/Http/Controllers/FrontPageController.php
+++ app/Http/Controllers/FrontPageController.php
@@ -7,7 +7,7 @@ class FrontPageController extends Controller
 {
     public function index(){
         $users = User::all();
-        return view('front')
+        return view('home')
             ->with('users', $users);
     }
 
--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -6,26 +6,29 @@
 @endsection
 
 @section('content')
-    <table class="table">
-        <thead class="thead-inverse">
-        <tr>
-            <th>#</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>Alamat</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            @foreach($users as $user)
-                <th scope="row">1</th>
-                <td>{{$user->name}}</td>
-                <td>{{$user->email}}</td>
-                <td>{{$user->alamat}}</td>
-            @endforeach
-        </tr>
-        </tbody>
-    </table>
+    @include('components.navbar')
+    <div class="container">
+        <table class="table">
+            <thead class="thead-inverse">
+            <tr>
+                <th>#</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Alamat</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                @foreach($users as $user)
+                    <th scope="row">1</th>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->alamat}}</td>
+                @endforeach
+            </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
 
 @section('script')
